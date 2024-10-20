ee89fb6c393ebc5bb242cf1dbad15929c53e3131
chung lee <lechung22092003@gmail.com>
2024-10-20 21:48:11 +0700

    edit class

--- modules/SuaLopHC.php
+++ modules/SuaLopHC.php
@@ -1,7 +1,7 @@
 <?php
 require_once './connect.php';
 $sid = $_GET['sid'];
-$select = "SELECT * FROM tblkhoa WHERE MaKhoa = $sid";
+$select = "SELECT * FROM tbllophc WHERE MaLopHC = $sid";
 
 $result = mysqli_query($conn, $select);
 $row = mysqli_fetch_assoc($result);
@@ -12,22 +12,39 @@ $row = mysqli_fetch_assoc($result);
     <div class="card">
         <div class="card-body">
 
-            <h5 class="card-title">Sửa Thông Tin Khoa</h5>
+            <h5 class="card-title">Sửa Thông Tin Lớp Hành Chính</h5>
             <!-- Vertical Form -->
             <form action="" method="post" class="row g-3">
                 <input type="hidden" name="sid" value="<?php echo $sid ?>">
                 <div class="col-12">
-                    <label for="TenKhoa" class="form-label">Tên Khoa</label>
-                    <input type="text" class="form-control" id="TenKhoa" name="TenKhoa" value="<?php echo $row['TenKhoa'] ?>">
+                    <label for="TenLopHC" class="form-label">Tên Lớp Hành Chính</label>
+                    <input type="text" class="form-control" id="TenLopHC" name="TenLopHC" value="<?php echo $row['TenLopHC'] ?>">
                 </div>
                 <div class="col-12">
-                    <label for="DienThoai" class="form-label">Số điện thoại</label>
-                    <input type="text" class="form-control" id="DienThoai" name="DienThoai" value="<?php echo $row['DienThoai'] ?>">
-                </div>
+    <label for="KhoaVien" class="form-label">Thuộc Khoa</label>
+    <div class="col-sm-10">
+        <select name="KhoaVien" class="form-select">
+            <?php
+            $sql = "SELECT * FROM tblkhoa";
+            $result = mysqli_query($conn, $sql);
+
+            while ($r = mysqli_fetch_array($result)) {
+                // So sánh MaKhoa hiện tại với MaKhoa trong bảng
+                $selected = ($r['MaKhoa'] == $row['MaKhoa']) ? "selected" : "";
+            ?>
+                <option value="<?php echo $r['MaKhoa']; ?>" <?php echo $selected; ?>>
+                    <?php echo htmlspecialchars($r['TenKhoa']); ?>
+                </option>
+            <?php
+            }
+            ?>
+        </select>
+    </div>
+</div>
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary">Cập nhật</button>
                     <button type="reset" class="btn btn-secondary">Xoá hết</button>
-                    <a href="index.php?modules=Khoa" type="reset" class="btn btn-success">Trở Về</a>
+                    <a href="index.php?modules=LopHanhChinh" type="reset" class="btn btn-success">Trở Về</a>
                 </div>
             </form><!-- Vertical Form -->
 
@@ -38,11 +55,11 @@ $row = mysqli_fetch_assoc($result);
     <?php
     
     if (isset($_SERVER['REQUEST_METHOD']) == 'POST') {
-        if (isset($_POST['TenKhoa']) && isset($_POST['DienThoai']) && isset($_POST['sid'])) {
-            $tenkhoa = $_POST['TenKhoa'];
-            $dienthoai = $_POST['DienThoai'];
+        if (isset($_POST['TenLopHC']) && isset($_POST['KhoaVien']) && isset($_POST['sid'])) {
+            $tenlophc = $_POST['TenLopHC'];
+            $khoavien = $_POST['KhoaVien'];
             $id = $_POST['sid'];
-            $update = "UPDATE tblkhoa SET TenKhoa ='$tenkhoa', DienThoai = '$dienthoai' WHERE MaKhoa='$id'";
+            $update = "UPDATE tbllophc SET TenLopHC ='$tenlophc', MaKhoa = '$khoavien' WHERE MaLopHC='$id'";
             $resultUpdate = mysqli_query($conn, $update);
             if ($resultUpdate) {
                 echo "<h4 class='alert alert-success'>Cập Nhật Thành Công</h4>";
